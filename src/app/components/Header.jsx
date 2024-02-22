b54ed8b6f1713a93ae5bd1601ad78bdcacebc42c
daviardev <jerson.silva99@hotmail.com>
2024-02-21 23:54:13 -0500

    Create link for the header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,15 +1,19 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Header () {
   return (
     <>
       <header className='flex justify-start pl-4'>
-        <Image
-          src='/images/tiktok-logo.png'
-          alt='TikTok Logo'
-          width={24}
-          height={24}
-        />
+        <Link href='/'>
+          <Image
+            src='/images/tiktok-logo.png'
+            alt='TikTok Logo'
+            width={24}
+            height={24}
+          />
+        </Link>
+
       </header>
     </>
   )
