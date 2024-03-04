8a3d5bc878dab2ab2a1294ed09075d04d69c9bb6
daviardev <jerson.silva99@hotmail.com>
2024-03-03 21:31:08 -0500

    Commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,7 @@ import FeedVideos from './components/FeedVideos'
 export default function Home () {
   return (
     <>
-      <div className='container__scroll'>
-        <FeedVideos />
-      </div>
+      <FeedVideos />
     </>
   )
 }
